ca7911fb0854e84950dc169f12ba18fe182cef1d
Lindsay Garza <lmwressell@aol.com>
2021-06-30 14:32:57 +0000

    Collecte sales data for last 5 markets

--- run.py
+++ run.py
@@ -64,8 +64,21 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """ """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
 def main():
+
     """ Runs all program functions"""
+
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
@@ -74,4 +87,6 @@ def main():
 
 
 print("Welcome to Love Sandiwches Data Automation\n")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
